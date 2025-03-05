c369b60db0a12c0d9c7319713f31113169b7f138
Mads Pedersen <mpd@dawnhealth.com>
2025-03-05 13:10:16 +0100

    resetting patch if major or minor changes

--- latest-patch-version.sh
+++ latest-patch-version.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# Get current commit
-current_commit=$(git rev-parse HEAD)
+branch_name=$1
 
 # Get the latest tag of the current commit if it exists
+current_commit=$(git rev-parse HEAD)
 latest_tag=$(git describe --tags --exact-match $current_commit 2>/dev/null)
 
-# If no tag is found for the current commit, find the latest tag.
+major=$(echo $branch_name | awk -F'.' '{print $1}' | sed 's/v//')
+minor=$(echo $branch_name | awk -F'.' '{print $2}')
+
 if [ -z "$latest_tag" ]; then
   latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-  # Extract the patch version from the tag (format is vX.Y.Z-envPostfix)
   patch_version=$(echo $latest_tag | awk -F'[.-]' '{print $3}')
-  # Increment the patch version
-  patch_version=$((patch_version + 1))
+  
+  # Extract the major, minor, and patch versions from the latest tag
+  IFS='.-' read -r tag_major tag_minor patch env <<< "${latest_tag#v}"
+  
+  # Check if the major or minor version has changed
+  if [ "$major" != "$tag_major" ] || [ "$minor" != "$tag_minor" ]; then
+    patch_version=0
+  else
+    patch_version=$((patch + 1))
+  fi
 else
-  # Extract the patch version from the tag (format is vX.Y.Z-envPostfix)
-  patch_version=$(echo $latest_tag | awk -F'[.-]' '{print $3}')
+  patch_version=0
 fi
 
 # Export the patch version as an environment variable
