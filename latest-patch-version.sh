9b17f1aa9e60f72e42510ac333c50fe269e8ec01
Mads Pedersen <mpd@dawnhealth.com>
2025-03-06 08:12:18 +0100

    supporting new tag with same commit id

--- latest-patch-version.sh
+++ latest-patch-version.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
+# Last part of the branch name. If the branch is 'release/1.4' this should be '1.4' (Build.SourceBranchName in Azure DevOps).
 branch_name=$1
 
 # Get the latest tag of the current commit if it exists
 current_commit=$(git rev-parse HEAD)
-latest_tag=$(git describe --tags --exact-match $current_commit 2>/dev/null)
+tags=$(git tag --contains $current_commit)
 
-major=$(echo $branch_name | awk -F'.' '{print $1}' | sed 's/v//')
-minor=$(echo $branch_name | awk -F'.' '{print $2}')
+# Find the latest tag based on version number
+# This is to support a tag having the same commit id as the previous tag
+latest_tag=""
+for tag in $tags; do
+  if [[ $tag =~ ^v[0-9]+\.[0-9]+\.[0-9]+- ]]; then
+    if [ -z "$latest_tag" ] || [ "$(printf '%s\n' "$tag" "$latest_tag" | sort -V | tail -n1)" == "$tag" ]; then
+      latest_tag=$tag
+    fi
+  fi
+done
 
+# Checks if latest_tag is empty
 if [ -z "$latest_tag" ]; then
   latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-  patch_version=$(echo $latest_tag | awk -F'[.-]' '{print $3}')
-  
-  # Extract the major, minor, and patch versions from the latest tag
-  IFS='.-' read -r tag_major tag_minor patch env <<< "${latest_tag#v}"
-  
-  # Check if the major or minor version has changed
-  if [ "$major" != "$tag_major" ] || [ "$minor" != "$tag_minor" ]; then
-    patch_version=0
-  else
-    patch_version=$((patch + 1))
-  fi
+  patch_version=$(echo $latest_tag | awk -F'[.-]' '{print $3}')  
 else
   patch_version=0
 fi
 
-# Export the patch version as an environment variable
+# Extract the major, minor, and patch versions from the latest tag
+IFS='.-' read -r tag_major tag_minor patch env <<< "${latest_tag#v}"
+
+# Extract the major and minor versions from the branch name
+major=$(echo $branch_name | awk -F'.' '{print $1}' | sed 's/v//')
+minor=$(echo $branch_name | awk -F'.' '{print $2}')
+
+# Check if the major or minor version has changed
+if [ "$major" != "$tag_major" ] || [ "$minor" != "$tag_minor" ]; then
+  patch_version=0
+else
+  patch_version=$((patch + 1))
+fi
+
 echo $patch_version
