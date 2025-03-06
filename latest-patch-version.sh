5e2969d6137daf9c8d7f00dbf7b7a953ba345da4
Mads Pedersen <mpd@dawnhealth.com>
2025-03-06 09:53:38 +0100

    updated comments

--- latest-patch-version.sh
+++ latest-patch-version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #
-# This script determines the latest patch version based on the branch name and the latest tag.
-# The script is intended to be used in a CI/CD pipeline to determine the next patch version for a release.
-# The script assumes that the branch name follows the format 'release/x.y' where x is the major version and y is the minor version.
-# The script also assumes that the tags follow the format 'vX.Y.Z-env' where X is the major version, Y is the minor version, and Z is the patch version. the -env is only for non production releases. 
+# The script:
+# - determines the latest patch version based on the branch name and the latest tag.
+# - is intended to be used in a CI/CD pipeline to determine the next patch version for a release.
+# - assumes that the branch name follows the format 'release/x.y' where x is the major version and y is the minor version.
+# - also assumes that the tags follow the format 'vX.Y.Z-env' where X is the major version, Y is the minor version, and Z is the patch version. the -env is only for non production releases. 
+# - does not support deploying out of order, e.g. deploying to production before deploying to QA. This will result in the patch version being incremented for the production release but when QA is being deployed it uses the same patch as production which it should not.
 #
 
 # Last part of the branch name. If the branch is 'release/1.4' this should be '1.4' (Build.SourceBranchName in Azure DevOps).
@@ -32,6 +34,7 @@ fi
 # Extract the major, minor, and patch versions from the latest tag
 IFS='.-' read -r tag_major tag_minor patch env <<< "${latest_tag#v}"
 
+# If the environment part of the latest tag is different from the environment we are deploying to, we do not increment the patch version
 if [ "-$env" != "$env_suffix" ]; then
   patch_version=$patch
 fi
@@ -48,7 +51,7 @@ elif [ "-$env" == "$env_suffix" ]; then
 fi
 
 # Check if the tag with the current patch version already exists and increment the patch version if it does.
-# This is to support deploying 1.5.2 to QA and then to Training and then afterwards deploying to QA again from the same branch.
+# This is to support deploying 1.5 to QA (resulting in 1.5.1-qa) and then to Training (resulting in 1.5.1-training) and then afterwards deploying to QA again from the same branch (resulting in 1.5.2-qa).
 while git rev-parse "v$major.$minor.$patch_version$env_suffix" >/dev/null 2>&1; do
   patch_version=$((patch + 1))
 done
